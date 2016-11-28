f75a10f59d093a4861280ab4e81b057028e82dd1
mykisskui <mykisskui@gmail.com>
2016-11-28 18:19:49 +0900

    修改导航

--- Mykisskui/blog/js/navscroll.js
+++ Mykisskui/blog/js/navscroll.js
@@ -1,1 +1,2 @@
-﻿
+﻿var nav = document.getElementById('topnav'), article = document.getElementsByTagName('article')[0];
+nav.style.cssText = 'width:'+(article.clientWidth - 80 )+'px;position:fixed;top:0;z-index:100;';
